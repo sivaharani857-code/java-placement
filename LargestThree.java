0d05dd453bf2af534d90955d6e51bbe6cce751a2
harini25it-stack <harini.s_25it@kgkite.ac.in>
2026-08-19 10:37:11 +0530

    find largest 3 numbers

---
+++ LargestThree.java
@@ -0,0 +1,12 @@
+class LargestThree {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 15;
+
+        if (a >= b && a >= c)
+            System.out.println(a);
+        else if (b >= a && b >= c)
+            System.out.println(b);
+        else
+            System.out.println(c);
+    }
+}
