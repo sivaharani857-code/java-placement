b722c0d675ca49cfdd67a1e7700dc8b14699dafb
harini25it-stack <harini.s_25it@kgkite.ac.in>
2026-08-19 09:52:11 +0530

    check positive negative or zero

---
+++ PositiveNegativeZero.java
@@ -0,0 +1,13 @@
+class PositiveNegativeZero {
+    public static void main(String[] args) {
+        int n = -5;
+
+        if (n > 0)
+            System.out.println("Positive");
+        else if (n < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
+
