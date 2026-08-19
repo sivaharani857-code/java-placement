6a477dec5765e3d617f119e4bda5abfc21efb3b1
harini25it-stack <harini.s_25it@kgkite.ac.in>
2026-08-19 10:39:47 +0530

    find smallest 3 numbers

---
+++ SmallestThree.java
@@ -0,0 +1,12 @@
+class SmallestThree {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 5;
+
+        if (a <= b && a <= c)
+            System.out.println(a);
+        else if (b <= a && b <= c)
+            System.out.println(b);
+        else
+            System.out.println(c);
+    }
+}
