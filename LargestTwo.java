baea3e10c6335c8cc2aeb144f1c2816ac7ad5c28
harini25it-stack <harini.s_25it@kgkite.ac.in>
2026-08-19 09:56:00 +0530

    find largest 2 number

---
+++ LargestTwo.java
@@ -0,0 +1,10 @@
+class LargestTwo {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        if (a > b)
+            System.out.println(a);
+        else
+            System.out.println(b);
+    }
+}
