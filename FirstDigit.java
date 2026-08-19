97dff4500ed5e2eef2bbb3a431fc82e16edf8100
harini25it-stack <harini.s_25it@kgkite.ac.in>
2026-08-19 21:37:21 +0530

    find first digit

---
+++ FirstDigit.java
@@ -0,0 +1,10 @@
+class FirstDigit {
+    public static void main(String[] args) {
+        int n = 12345;
+
+        while (n >= 10)
+            n /= 10;
+
+        System.out.println(n);
+    }
+}
