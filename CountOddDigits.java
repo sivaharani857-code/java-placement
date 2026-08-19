d5b6bf65b60213d3341139f12929862aee3769ff
harini25it-stack <harini.s_25it@kgkite.ac.in>
2026-08-19 21:45:29 +0530

    count odd digits

---
+++ CountOddDigits.java
@@ -0,0 +1,17 @@
+class CountOddDigits {
+    public static void main(String[] args) {
+        int n = 123456;
+        int count = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit % 2 != 0)
+                count++;
+
+            n /= 10;
+        }
+
+        System.out.println(count);
+    }
+}
