8e48674b082b071c04d60490999165ef010b79ca
harini25it-stack <harini.s_25it@kgkite.ac.in>
2026-08-19 21:33:17 +0530

    find automorphic number

---
+++ Automorphic.java
@@ -0,0 +1,11 @@
+class Automorphic {
+    public static void main(String[] args) {
+        int n = 25;
+        int square = n * n;
+
+        if (square % 100 == n)
+            System.out.println("Automorphic Number");
+        else
+            System.out.println("Not Automorphic Number");
+    }
+}
