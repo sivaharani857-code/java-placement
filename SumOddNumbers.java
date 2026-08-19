205294fd82e83b670eb87957ea9654c8426f3cd8
harini25it-stack <harini.s_25it@kgkite.ac.in>
2026-08-19 22:15:36 +0530

    Sum of Odd Numbers up to N

---
+++ SumOddNumbers.java
@@ -0,0 +1,11 @@
+class SumOddNumbers {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i += 2)
+            sum += i;
+
+        System.out.println(sum);
+    }
+}
